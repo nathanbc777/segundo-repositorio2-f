284602cf2404da583258cfd96091374660de57bc
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2025-06-26 22:05:36 -0300

    aula 3 26/06

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ function mostraPalavraChave() {
     //alert("fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave')
+    const palavras = processaTexto(texto);
 
-    campoResultado.textContent = texto;
+    campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto() {
+    //let palavras = texto.split(/\s+/);  "retirar espaços"
+    //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
+      let palavras = texto.split("/\p{L}+/u/"); /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    let frequencias = [];
+
+    for ( let i in palavras) {
+        frequencias[i] = 0;
+        for ( let j in palavras) {
+            if(palavras[i] == palavras[j]) {
+                frequecias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
+    return palavras
 }
