8e6c864476288a08282d35ab3cdc0ee52de249d1
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2025-07-31 21:07:45 -0300

    aula 31/07

--- script.js
+++ script.js
@@ -8,15 +8,21 @@ function mostraPalavraChave() {
     //alert("fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave')
-    const palavras = processaTexto(texto);
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto() {
     //let palavras = texto.split(/\s+/);  "retirar espaços"
     //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
       let palavras = texto.split("/\p{L}+/u/"); /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+      for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+      }
+
+      palavras = tiraPalavrasRuins(palavras);
 
       const frequencias = contaFrequencias(palavras);
 
